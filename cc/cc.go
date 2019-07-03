f7f45de9c09e52288ddde8bf848b76dbfc7d76c8
Ramesh Kodam <rameko@codeaurora.org>
2019-07-02 17:45:50 -0700

    Revert "Add support for proprietary compiler"
    This reverts commit 1d44245c6e586186c567512a5b8f6e2422d451fc.
    
    Change-Id: I12d369ddd6f8ae795bf431e2744fcd4a37d56a63
    Signed-off-by: Ramesh Kodam <rameko@codeaurora.org>

--- cc/cc.go
+++ cc/cc.go
@@ -150,7 +150,6 @@ type Flags struct {
 	SystemIncludeFlags []string
 
 	Toolchain config.Toolchain
-	Sdclang   bool
 	Tidy      bool
 	Coverage  bool
 	SAbiDump  bool
@@ -180,9 +179,6 @@ type ObjectLinkerProperties struct {
 type BaseProperties struct {
 	// Deprecated. true is the default, false is invalid.
 	Clang *bool `android:"arch_variant"`
-
-	// compile module with SDLLVM instead of AOSP LLVM
-	Sdclang *bool `android:"arch_variant"`
 
 	// Minimum sdk version supported when compiling against the ndk
 	Sdk_version *string
@@ -928,7 +924,6 @@ func (c *Module) GenerateAndroidBuildActions(actx android.ModuleContext) {
 
 	flags := Flags{
 		Toolchain: c.toolchain(ctx),
-		Sdclang:   c.sdclang(ctx),
 	}
 	if c.compiler != nil {
 		flags = c.compiler.compilerFlags(ctx, flags, deps)
@@ -1397,21 +1392,6 @@ func BeginMutator(ctx android.BottomUpMutatorContext) {
 	if c, ok := ctx.Module().(*Module); ok && c.Enabled() {
 		c.beginMutator(ctx)
 	}
-}
-
-func (c *Module) sdclang(ctx BaseModuleContext) bool {
-	sdclang := Bool(c.Properties.Sdclang)
-
-	// SDLLVM is not for host build
-	if ctx.Host() {
-		return false
-	}
-
-	if c.Properties.Sdclang == nil && config.SDClang {
-		return true
-	}
-
-	return sdclang
 }
 
 // Whether a module can link to another module, taking into
