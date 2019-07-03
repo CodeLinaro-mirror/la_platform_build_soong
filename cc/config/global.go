ba2a8c63dd69bca55b5b04a9996ff7b0225cfe4b
Ramesh Kodam <rameko@codeaurora.org>
2019-07-02 17:40:19 -0700

    Revert "Fix compilation error for Android Q for sdclang.json path"
    This reverts commit f07f8149f29f7cc11a881a454edfdcec599a97f8.
    
    Change-Id: Id8e1d2124aea2d2145d12a619c7439541fd72ef2
    Signed-off-by: Ramesh Kodam <rameko@codeaurora.org>

--- cc/config/global.go
+++ cc/config/global.go
@@ -18,7 +18,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path"
 	"strconv"
 	"strings"
 
@@ -251,7 +250,6 @@ func setSdclangVars() {
 	sdclangFlags2 := ""
 
 	product := android.SdclangEnv["TARGET_PRODUCT"]
-	androidRoot := android.SdclangEnv["TOP"]
 	aeConfigPath := android.SdclangEnv["SDCLANG_AE_CONFIG"]
 	sdclangConfigPath := android.SdclangEnv["SDCLANG_CONFIG"]
 
@@ -260,8 +258,7 @@ func setSdclangVars() {
 	}
 
 	// Load AE config file and set AE flag
-	aeConfigFile := path.Join(androidRoot, aeConfigPath)
-	if file, err := os.Open(aeConfigFile); err == nil {
+	if file, err := os.Open(aeConfigPath); err == nil {
 		decoder := json.NewDecoder(file)
 		aeConfig := sdclangAEConfig{}
 		if err := decoder.Decode(&aeConfig); err == nil {
@@ -272,9 +269,8 @@ func setSdclangVars() {
 	}
 
 	// Load SD Clang config file and set SD Clang variables
-	sdclangConfigFile := path.Join(androidRoot, sdclangConfigPath)
 	var sdclangConfig interface{}
-	if file, err := os.Open(sdclangConfigFile); err == nil {
+	if file, err := os.Open(sdclangConfigPath); err == nil {
 		decoder := json.NewDecoder(file)
                 // Parse the config file
 		if err := decoder.Decode(&sdclangConfig); err == nil {
