30395fac7938627a439a5884934d90eef4aa2f43
Swapneel Mashalkar <smashalk@codeaurora.org>
2021-10-25 16:21:30 +0530

    [ES]: Soong changes for ES
    Change-Id: I3200ac4f272228647a2a101b55a35bbc07bc7ea3

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -164,6 +164,7 @@ var BannerVars = []string{
 	"PRODUCT_SOONG_NAMESPACES",
 	"SOONG_SDK_SNAPSHOT_PREFER",
 	"SOONG_SDK_SNAPSHOT_VERSION",
+	"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 }
 
 func Banner(make_vars map[string]string) string {
@@ -221,6 +222,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 
 		// Whether to enable the network during the build
 		"BUILD_BROKEN_USES_NETWORK",
+		"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 
 		// Extra environment variables to be exported to ninja
 		"BUILD_BROKEN_NINJA_USES_ENV_VARS",
@@ -275,6 +277,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 			env.Set(name, makeVars[name])
 		}
 	}
+	env.Set("BOARD_SUPPORTS_RAMDISK_EARLY_INIT", makeVars["BOARD_SUPPORTS_RAMDISK_EARLY_INIT"])
 
 	config.SetKatiArgs(strings.Fields(makeVars["KATI_GOALS"]))
 	config.SetNinjaArgs(strings.Fields(makeVars["NINJA_GOALS"]))
