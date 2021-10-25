869d47570e84d91445ba428ee11f29eec5a9519f
Swapneel Mashalkar <smashalk@codeaurora.org>
2021-10-25 16:21:30 +0530

    [ES]: Soong changes for ES
    Added support for early services
    
    Change-Id: I3200ac4f272228647a2a101b55a35bbc07bc7ea3
    Signed-off-by: Mamata Lingala <quic_lingala@quicinc.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -165,6 +165,7 @@ var BannerVars = []string{
 	"BUILD_ID",
 	"OUT_DIR",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
+	"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 }
 
 func Banner(make_vars map[string]string) string {
@@ -228,6 +229,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 
 		// Whether to enable the network during the build
 		"BUILD_BROKEN_USES_NETWORK",
+		"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 
 		// Extra environment variables to be exported to ninja
 		"BUILD_BROKEN_NINJA_USES_ENV_VARS",
@@ -291,6 +293,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 			env.Set(name, makeVars[name])
 		}
 	}
+	env.Set("BOARD_SUPPORTS_RAMDISK_EARLY_INIT", makeVars["BOARD_SUPPORTS_RAMDISK_EARLY_INIT"])
 
 	config.SetKatiArgs(strings.Fields(makeVars["KATI_GOALS"]))
 	config.SetNinjaArgs(strings.Fields(makeVars["NINJA_GOALS"]))
