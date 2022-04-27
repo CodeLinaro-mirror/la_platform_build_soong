244fa15215757a1425237ec851625c0d340a8a92
Kiran Pachava <quic_kpachava@quicinc.com>
2022-04-28 00:27:41 +0530

    Revert "[ES]: Soong changes for ES"
    This reverts commit 30395fac7938627a439a5884934d90eef4aa2f43.

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -164,7 +164,6 @@ var BannerVars = []string{
 	"PRODUCT_SOONG_NAMESPACES",
 	"SOONG_SDK_SNAPSHOT_PREFER",
 	"SOONG_SDK_SNAPSHOT_VERSION",
-	"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 }
 
 func Banner(make_vars map[string]string) string {
@@ -222,7 +221,6 @@ func runMakeProductConfig(ctx Context, config Config) {
 
 		// Whether to enable the network during the build
 		"BUILD_BROKEN_USES_NETWORK",
-		"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 
 		// Extra environment variables to be exported to ninja
 		"BUILD_BROKEN_NINJA_USES_ENV_VARS",
@@ -277,7 +275,6 @@ func runMakeProductConfig(ctx Context, config Config) {
 			env.Set(name, makeVars[name])
 		}
 	}
-	env.Set("BOARD_SUPPORTS_RAMDISK_EARLY_INIT", makeVars["BOARD_SUPPORTS_RAMDISK_EARLY_INIT"])
 
 	config.SetKatiArgs(strings.Fields(makeVars["KATI_GOALS"]))
 	config.SetNinjaArgs(strings.Fields(makeVars["NINJA_GOALS"]))
