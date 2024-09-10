a071f5afc9fd3ac07b13328d4a45ce01f7f64092
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-09-10 16:51:27 +0530

    Revert "Revert^2 "Make embedded_launcher true by default""
    This reverts commit f09101e78da3aafa7f2a28959d0d3366f4767aa1.
    
    Change-Id: I7570be23002b47ca81b6fd619a2060cd21c91445
    (cherry picked from commit cdb7eac7214cbd156a2ec3bac41f972dc42e8946)
    (cherry picked from commit d2cbe4155d35e6792f99534d62cf8dee919fad22)

--- python/binary.go
+++ python/binary.go
@@ -189,7 +189,7 @@ func (p *PythonBinaryModule) HostToolPath() android.OptionalPath {
 }
 
 func (p *PythonBinaryModule) isEmbeddedLauncherEnabled() bool {
-	return BoolDefault(p.properties.Embedded_launcher, true)
+	return Bool(p.properties.Embedded_launcher)
 }
 
 func (b *PythonBinaryModule) autorun() bool {
