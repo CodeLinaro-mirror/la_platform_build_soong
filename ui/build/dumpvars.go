89acb441ff92dd824857c8f3c70bc84887a73771
huayzhon <huayzhon@qti.qualcomm.com>
2025-11-04 14:30:58 +0800

    build:soong: change for Early service
    Change-Id: If6be12b4b7e9e1453381e38113df27f1c939482d

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// Changes from Qualcomm Technologies, Inc. are provided under the following license:
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. 
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 
 package build
 
@@ -170,6 +173,7 @@ var BannerVars = []string{
 	"BUILD_ID",
 	"OUT_DIR",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
+	"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 }
 
 func Banner(config Config, make_vars map[string]string) string {
@@ -239,6 +243,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 
 		// Whether to enable the network during the build
 		"BUILD_BROKEN_USES_NETWORK",
+		"BOARD_SUPPORTS_RAMDISK_EARLY_INIT",
 
 		// Extra environment variables to be exported to ninja
 		"BUILD_BROKEN_NINJA_USES_ENV_VARS",
@@ -304,6 +309,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 			env.Set(name, makeVars[name])
 		}
 	}
+	env.Set("BOARD_SUPPORTS_RAMDISK_EARLY_INIT", makeVars["BOARD_SUPPORTS_RAMDISK_EARLY_INIT"])
 
 	config.SetKatiArgs(strings.Fields(makeVars["KATI_GOALS"]))
 	config.SetNinjaArgs(strings.Fields(makeVars["NINJA_GOALS"]))
